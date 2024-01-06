44767bac75bfd4a4b42d1301a26ff9632861d9e8
romrosmay <99548008+romrosmay@users.noreply.github.com>
2024-01-06 01:59:55 +0100

    canvi footer

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -2,12 +2,12 @@ import ContactMap from "@/components/contactMap";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-200 text-gray-700 body-font">
+    <footer className="bg-slate-950 text-gray-700 body-font">
       <ContactMap
         title="Posa't en Contacte"
-        description="Si tens alguna pregunta, no dubtis en contactar-nos. Estarem encantats d'atendre't."
+        description="Si tens alguna pregunta, no dubtis en contactar-nos. <br/>Estarem encantats d'atendre't."
         // color del fons del formulari: fosc blau
-        backgroundColor="#e5e7eb"
+        backgroundColor="bg-slate-900"
         messages={{
           nameRequired: "El nom és obligatori.",
           emailRequired: "L'email és obligatori.",
@@ -22,24 +22,22 @@ export default function Footer() {
           lloc: "Lo Bram sccl",
         }}
       />
-      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+      <div className="container px-5 py-0  mx-auto flex items-center sm:flex-row flex-col">
+        <a className="flex title-font font-medium items-center md:justify-start justify-center text-slate-200">
           <span className="ml-3 text-xl">
             Escola d'Ensenyances de Gurdjieff
           </span>
         </a>
-        <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
+        <p className="text-sm text-slate-400 sm:ml-6 sm:mt-0 mt-4">
           © {new Date().getFullYear()} Escola d'Ensenyances de Gurdjieff - Tots
           els drets reservats
         </p>
-        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <a href="/politica-de-privacitat" className="text-gray-500">
-            Política de Privacitat
-          </a>
-          <a href="/avis-legal" className="ml-4 text-gray-500">
+        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start text-slate-300">
+          <a href="/politica-de-privacitat">Política de Privacitat</a>
+          <a href="/avis-legal" className="ml-4">
             Avís Legal
           </a>
-          <a href="/contacte" className="ml-4 text-gray-500">
+          <a href="/contacte" className="ml-4">
             Contacte
           </a>
         </span>
--- components/contactMap.tsx
+++ components/contactMap.tsx
@@ -7,6 +7,7 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import { IContactForm, sendContact } from "@/app/lib/actions";
 import toast, { Toaster } from "react-hot-toast";
+import { cn } from "@/lib/utils";
 
 interface ContactMapProps {
   backgroundColor?: string;
@@ -89,27 +90,34 @@ export default function ContactMap(props: ContactMapProps) {
 
   const mapa_url = `https://maps.google.com/maps?hl=${mapa.idioma}&q=${mapa.lloc}&ie=UTF8&t=roadmap&z=12&iwloc=B&output=embed`;
 
+  const main_div_class = cn(
+    "flex flex-col md:flex-rows w-full",
+    backgroundColor
+  );
+
   return (
-    <div
-      className="flex flex-col md:flex-row-reverse  w-full"
-      style={{ backgroundColor: backgroundColor }}
-    >
-      <Toaster position="top-center" reverseOrder={false} />
-      {/* Columna de Text */}
-      <div className="flex flex-col justify-start mt-4 items-center md:w-2/3 text-center">
-        <div className="flex flex-col md:flex-row w-full">
-          <div className="bg-white p-8 rounded-lg shadow-lg mx-auto my-10 ">
-            <h2 className="text-2xl font-semibold text-center mb-6">
-              {title || "Posa't en Contacte"}
-            </h2>
-            <p
-              className={`mb-6 lg:px-10 md:px-5 sm:px-0 py-1  sm:text-normal  text-lg  `}
-              dangerouslySetInnerHTML={{
-                __html: description || "Descripció",
-              }}
-            ></p>
-            <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-              <div>
+    <div className={main_div_class}>
+      <div className="flex flex-col md:flex-row justify-start mt-4">
+        <div className="items-center md:w-1/3 text-center">
+          <Toaster position="top-center" reverseOrder={false} />
+          <h2 className=" text-slate-300 text-xl font-semibold text-center mb-6">
+            {title || "Posa't en Contacte"}
+          </h2>
+          <p
+            className={`text-slate-400 mb-6 lg:px-10 md:px-5 sm:px-0 py-1 text-justify  sm:text-normal text-base  `}
+            dangerouslySetInnerHTML={{
+              __html: description || "Descripció",
+            }}
+          ></p>
+        </div>
+
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col justify-start items-center md:w-2/3 text-center"
+        >
+          <div className="flex flex-col md:flex-row w-full">
+            <div className="md:w-1/2 p-4">
+              <div className="pb-4">
                 <input
                   type="text"
                   {...register("name", { required: messages.nameRequired })}
@@ -132,66 +140,65 @@ export default function ContactMap(props: ContactMapProps) {
                   <p className="text-red-500">{errors.email.message}</p>
                 )}
               </div>
-
-              <div>
-                <textarea
-                  {...register("message", {
-                    required: messages.messageRequired,
-                  })}
-                  placeholder={formulari.text_missatge}
-                  className="w-full px-4 py-2 border rounded-lg focus:ring focus:ring-opacity-50"
-                  rows={4}
-                ></textarea>
-                {errors.message && (
-                  <p className="text-red-500">{errors.message.message}</p>
-                )}
-              </div>
-
-              <div className="flex items-start">
-                <input
-                  type="checkbox"
-                  {...register("consent", {
-                    required: messages.consentRequired,
-                  })}
-                  className="w-4 h-4 mt-1"
-                />
-                <label className="ml-2 text-sm">
-                  <span
-                    dangerouslySetInnerHTML={{
-                      __html:
-                        formulari.text_politica ||
-                        "Accepto la política de privacitat i consenteixo la recopilació de les meves dades a través d'aquest formulari.",
-                    }}
-                  ></span>
-                </label>
-              </div>
+            </div>
+            <div className="md:w-1/2 p-4">
+              <textarea
+                {...register("message", {
+                  required: messages.messageRequired,
+                })}
+                placeholder={formulari.text_missatge}
+                className="w-full px-4 py-2 border rounded-lg focus:ring focus:ring-opacity-50"
+                rows={4}
+              ></textarea>
+              {errors.message && (
+                <p className="text-red-500">{errors.message.message}</p>
+              )}
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row w-full items-stretch">
+            <div className="flex items-start w-4/6">
+              <input
+                type="checkbox"
+                {...register("consent", {
+                  required: messages.consentRequired,
+                })}
+                className="w-4 h-4 mt-1"
+              />
+              <label className="ml-2 text-xs text-left text-slate-300">
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html:
+                      formulari.text_politica ||
+                      "Accepto la política de privacitat i consenteixo la recopilació de les meves dades a través d'aquest formulari.",
+                  }}
+                ></span>
+              </label>
               {errors.consent && (
                 <p className="text-red-500">{errors.consent.message}</p>
               )}
+            </div>
 
-              <button
-                type="submit"
-                className="w-full px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
-              >
-                {formulari.text_enviar}
-              </button>
-            </form>
+            <button
+              type="submit"
+              className="w-1/6 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
+            >
+              {formulari.text_enviar}
+            </button>
           </div>
-        </div>
+        </form>
       </div>
-      <div className="md:w-1/3 ">
+      <div className="h-full">
         <div
-          className="flex flex-col items-stretch relative w-full"
+          className="flex flex-col items-stretch relative w-full px-4 pt-4 pb-0"
           style={{
-            height: "60vh",
-            minHeight: "100%",
+            height: "25vh",
           }}
         >
           <iframe
             width="100%"
             height="100%"
             src={mapa_url}
-            className="border-0"
+            className="border-0 rounded-sm"
             allowFullScreen
           ></iframe>
         </div>
@@ -199,3 +206,6 @@ export default function ContactMap(props: ContactMapProps) {
     </div>
   );
 }
+
+/*
+ */
